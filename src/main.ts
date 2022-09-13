b5d5ed30d7fd04d8434f15e3031d66aa689e72a0
Teten Nugraha <teten.nugraha18@gmail.com>
2022-09-14 06:54:35 +0700

    setup env and port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+
+  Logger.log(`Server started running on http://localhost:${port}`);
 }
 bootstrap();
